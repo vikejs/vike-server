1fe0832962842fc64ae17447c99ee83f2b9b6209
Romuald Brillout <git@brillout.com>
2025-09-29 17:08:38 +0200

    try pageContext.runtime

--- examples/express-react/pages/index/+Page.jsx
+++ examples/express-react/pages/index/+Page.jsx
@@ -2,8 +2,12 @@ export default Page
 
 import React from 'react'
 import { Counter } from './Counter'
+import { usePageContext } from 'vike-react/usePageContext'
 
 function Page() {
+  const pageContext = usePageContext()
+  console.log('pageContext.runtime.req', pageContext.runtime.req)
+  console.log('pageContext.runtime.res', pageContext.runtime.res)
   return (
     <>
       <h1>Welcome</h1>
